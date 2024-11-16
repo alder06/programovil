44a4211c1d1fad0cb74b4af57800af91ae89fd85
voyager0812 <134560843+voyager0812@users.noreply.github.com>
2024-11-16 15:22:14 -0300

    Add files via upload
    Iniciar los comandos y instalar todo

---
+++ environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA5naFtbwcWThzcPsnwyaLb-y-gaAx1BxY",
+    authDomain: "proyectmobiledanks.firebaseapp.com",
+    projectId: "proyectmobiledanks",
+    storageBucket: "proyectmobiledanks.appspot.com",
+    messagingSenderId: "815740163033",
+    appId: "1:815740163033:web:55b66042cb8b1ce570ffc8"
+  }
+};
